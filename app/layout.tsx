d52fb13fdaaa3a72707fc2959c6275d3f4a53ca3
serverbbq <76498501+ServerBBQ@users.noreply.github.com>
2025-10-13 20:11:07 -0400

    explicitly set global favicon to prevent each tool from having its own

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import nextConfig from "@/next.config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "3rd party Javelin web tools",
   description: "A 3rd party repository of Javelin web tools",
+  icons: {
+    icon: `${nextConfig.basePath}/favicon.ico`,
+  },
 };
 
 export default function RootLayout({
